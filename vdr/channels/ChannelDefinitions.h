4905d05d01cafcbfbd189bc8d7848d8be14c0d00
Garrett Brown <garbearucla@gmail.com>
2014-07-29 23:23:16 -0700

    Rename tags used in channels.xml

--- vdr/channels/ChannelDefinitions.h
+++ vdr/channels/ChannelDefinitions.h
@@ -20,40 +20,40 @@
  */
 #pragma once
 
-#define CHANNEL_XML_ROOT              "channels"
+#define CHANNEL_XML_ROOT                  "channels"
 
-#define CHANNEL_XML_ELM_CHANNEL       "channel"
-#define CHANNEL_XML_ELM_SEPARATOR     "separator"
-#define CHANNEL_XML_ELM_VPID          "video-pid"     // Video stream packet ID
-#define CHANNEL_XML_ELM_APID          "audio-pid"     // Audio stream packet ID
-#define CHANNEL_XML_ELM_APIDS         "audio-pids"
-#define CHANNEL_XML_ELM_DPID          "data-pid"      // Data stream packet ID
-#define CHANNEL_XML_ELM_DPIDS         "data-pids"
-#define CHANNEL_XML_ELM_SPID          "subtitle-pid"  // Subtitle stream packet ID
-#define CHANNEL_XML_ELM_SPIDS         "subtitle-pids"
-#define CHANNEL_XML_ELM_TPID          "teletext-pid"  // Teletext stream packet ID
-#define CHANNEL_XML_ELM_CAIDS         "ca-ids"
-#define CHANNEL_XML_ELM_CAID          "ca-id"
-#define CHANNEL_XML_ELM_TRANSPONDER   "transponder"
+#define CHANNEL_XML_ELM_CHANNEL           "channel"
+#define CHANNEL_XML_ELM_SEPARATOR         "separator"
+#define CHANNEL_XML_ELM_VPID              "videostream"     // Video stream packet ID
+#define CHANNEL_XML_ELM_APID              "pid"             // Audio stream packet ID
+#define CHANNEL_XML_ELM_APIDS             "audiostreams"
+#define CHANNEL_XML_ELM_DPID              "pid"             // Data stream packet ID
+#define CHANNEL_XML_ELM_DPIDS             "datastreams"
+#define CHANNEL_XML_ELM_SPID              "pid"             // Subtitle stream packet ID
+#define CHANNEL_XML_ELM_SPIDS             "subtitlestreams"
+#define CHANNEL_XML_ELM_TPID              "teletextstream"  // Teletext stream packet ID
+#define CHANNEL_XML_ELM_CAIDS             "caids"
+#define CHANNEL_XML_ELM_CAID              "caid"
+#define CHANNEL_XML_ELM_TRANSPONDER       "transponder"
 
-#define CHANNEL_XML_ATTR_NAME         "name"
-#define CHANNEL_XML_ATTR_SHORTNAME    "shortname"
-#define CHANNEL_XML_ATTR_PROVIDER     "provider"
-#define CHANNEL_XML_ATTR_NUMBER       "number"
-#define CHANNEL_XML_ATTR_PPID         "pcr-pid"       // Program clock reference packet ID
-#define CHANNEL_XML_ATTR_VTYPE        "video-type"    // Video stream type
-#define CHANNEL_XML_ATTR_ALANG        "audio-lang"    // Audio stream language
-#define CHANNEL_XML_ATTR_ATYPE        "audio-type"    // Audio stream type
-#define CHANNEL_XML_ATTR_DLANG        "data-lang"     // Data stream language
-#define CHANNEL_XML_ATTR_DTYPE        "data-type"     // Data stream type
-#define CHANNEL_XML_ATTR_SLANG        "subtitle-lang" // Subtitle stream language
+#define CHANNEL_XML_ATTR_NAME             "name"
+#define CHANNEL_XML_ATTR_SHORTNAME        "shortname"
+#define CHANNEL_XML_ATTR_PROVIDER         "provider"
+#define CHANNEL_XML_ATTR_NUMBER           "number"
+#define CHANNEL_XML_ATTR_PPID             "pcr-pid"  // Program clock reference packet ID
+#define CHANNEL_XML_ATTR_VTYPE            "type"     // Video stream type
+#define CHANNEL_XML_ATTR_ALANG            "lang"     // Audio stream language
+#define CHANNEL_XML_ATTR_ATYPE            "type"     // Audio stream type
+#define CHANNEL_XML_ATTR_DLANG            "lang"     // Data stream language
+#define CHANNEL_XML_ATTR_DTYPE            "type"     // Data stream type
+#define CHANNEL_XML_ATTR_SLANG            "lang"     // Subtitle stream language
 
 #define CHANNEL_SOURCE_XML_ATTR_SOURCE    "source"
-#define CHANNEL_SOURCE_XML_ATTR_POSITION  "satellite-position" // TODO: Add this property to cTransponder
+#define CHANNEL_SOURCE_XML_ATTR_POSITION  "satelliteposition" // TODO: Add this property to cTransponder
 
-#define CHANNEL_ID_XML_ATTR_NID  "network-id"
-#define CHANNEL_ID_XML_ATTR_TSID "transport-stream-id"
-#define CHANNEL_ID_XML_ATTR_SID  "service-id"
+#define CHANNEL_ID_XML_ATTR_NID           "networkid"
+#define CHANNEL_ID_XML_ATTR_TSID          "transportstreamid"
+#define CHANNEL_ID_XML_ATTR_SID           "serviceid"
 
 #define TRANSPONDER_XML_ATTR_FREQUENCY    "frequency"
 #define TRANSPONDER_XML_ATTR_SYMBOL_RATE  "symbolrate"
