4d5c91b919df3ef95c2c6a5766db232f907ff5aa
Garrett Brown <garbearucla@gmail.com>
2014-09-08 18:48:32 -0700

    Fix c/p error

--- vdr/devices/subsystems/DeviceReceiverSubsystem.cpp
+++ vdr/devices/subsystems/DeviceReceiverSubsystem.cpp
@@ -245,7 +245,7 @@ bool cDeviceReceiverSubsystem::OpenResources(const ChannelPtr& channel, PidResou
 
 PidResourcePtr cDeviceReceiverSubsystem::OpenResourceInternal(uint16_t pid, STREAM_TYPE streamType)
 {
-  PidResourcePtr newResource = GetOpenResource(CreateResource(pid, streamType));
+  PidResourcePtr newResource = CreateResource(pid, streamType);
   PidResourcePtr existingResource = GetOpenResource(newResource);
 
   if (existingResource)
