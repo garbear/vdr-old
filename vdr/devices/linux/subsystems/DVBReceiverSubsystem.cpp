01c3ffe753cb39b54c769278a340048d783e642f
Lars Op den Kamp <lars@opdenkamp.eu>
2014-11-01 16:42:27 +0100

    Revert "attempt to call DMX_START after setting the PES filter. DMX_IMMEDIATE_START should do this, but the tuner driver that we're having here may be buggy..." Geniatech's example code isn't doing this either
    This reverts commit 4ab7e80b8d54db89cd02be823619948d65d72989.

--- vdr/devices/linux/subsystems/DVBReceiverSubsystem.cpp
+++ vdr/devices/linux/subsystems/DVBReceiverSubsystem.cpp
@@ -112,12 +112,6 @@ bool cDvbReceiverResource::Open(void)
       Close();
       return false;
     }
-
-    if (ioctl(m_handle, DMX_START, 0)) {
-      esyslog("Couldn't open %s: ioctl failed", ToString().c_str());
-      Close();
-      return false;
-    }
   }
 
   // TODO: Magic code that makes everything work on Android
