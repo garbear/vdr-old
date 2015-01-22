95ce9cc4fe13784e43c4d3f39efd3b6122261ec9
Lars Op den Kamp <lars@opdenkamp.eu>
2015-01-22 02:54:08 +0100

    log the tsid in the pmt filter

--- vdr/dvb/filters/PMT.cpp
+++ vdr/dvb/filters/PMT.cpp
@@ -149,7 +149,7 @@ ChannelPtr cPmt::CreateChannel(/* const */ SI::PMT& pmt, uint16_t tsid) const //
 
   // Log a comma-separated list of streams we found in the channel
   stringstream logStreams;
-  logStreams << "PMT: Found channel with SID=" << pmt.getServiceId() << " and streams: " << channel->GetVideoStream().vpid << " (Video)";
+  logStreams << "PMT: Found channel with TSID=" << tsid << " SID=" << pmt.getServiceId() << " and streams: " << channel->GetVideoStream().vpid << " (Video)";
   for (vector<AudioStream>::const_iterator it = channel->GetAudioStreams().begin(); it != channel->GetAudioStreams().end(); ++it)
     logStreams << ", " << it->apid << " (Audio)";
   for (vector<DataStream>::const_iterator it = channel->GetDataStreams().begin(); it != channel->GetDataStreams().end(); ++it)
