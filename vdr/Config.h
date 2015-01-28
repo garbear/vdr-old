1fb7677c858075c3d3113a611fc3b7f3ea4394b5
Lars Op den Kamp <lars@opdenkamp.eu>
2015-01-28 01:08:05 +0100

    bump to 2.0.4-73

--- vdr/Config.h
+++ vdr/Config.h
@@ -31,7 +31,7 @@ namespace VDR
 {
 
 // VDR's own version number:
-#define VDRVERSION  "2.0.4-72"
+#define VDRVERSION  "2.0.4-73"
 #define VDRVERSNUM   20004  // Version * 10000 + Major * 100 + Minor
 
 #define MAXPRIORITY       99
